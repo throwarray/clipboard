3d87b3eadfa02767bce6d61658709c4466c4cf21
throwarray <throwarray@local>
2018-06-21 11:50:33 -0400

    tabs

--- client.lua
+++ client.lua
@@ -4,32 +4,38 @@ local SETTING_CLIPBOARD = false
 RegisterNUICallback('message', function (data)
 	if data == nil then return
 
-    elseif data.type == 'SETTING_CLIPBOARD' then
-        SETTING_CLIPBOARD = true
+	elseif data.type == 'SETTING_CLIPBOARD' then
+		SETTING_CLIPBOARD = true
 
-    elseif data.type == 'SET_CLIPBOARD' then
-	    SETTING_CLIPBOARD = false
+	elseif data.type == 'SET_CLIPBOARD' then
+		SETTING_CLIPBOARD = false
 
-        if (CALLBACK) then
-	        CALLBACK(not data.payload)
-	        CALLBACK = nil
-        end
+		if (CALLBACK) then
+			CALLBACK(not data.payload)
+			CALLBACK = nil
+		end
 
-        SetTimeout(17, function () SetNuiFocus(false, false) end)
+		SetTimeout(17, function ()
+			SetNuiFocus(false, false)
+		end)
 	end
 end)
 
 function SettingClipboard ()
-    return SETTING_CLIPBOARD
+	return SETTING_CLIPBOARD
 end
 
 function SetClipboard (text, cb)
-    if SettingClipboard() then
-        if cb then cb(true) end
-    else
-        SETTING_CLIPBOARD = true
-        CALLBACK = cb
-        SetNuiFocus(true, false)
-    	SendNUIMessage({ type = 'SET_CLIPBOARD', payload = text, meta = 1 })
-    end
+	if SettingClipboard() then
+		if cb then cb(true) end
+	else
+		SETTING_CLIPBOARD = true
+		CALLBACK = cb
+		SetNuiFocus(true, false)
+		SendNUIMessage({
+			type = 'SET_CLIPBOARD',
+			payload = text,
+			meta = 1
+		})
+	end
 end
